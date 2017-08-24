0e6326924485ae3cfc9e35a8b7eb73ca5c84defc
mhtbhavsar <mohit.bhavsar@coderize.in>
2017-08-24 12:03:33 +0530

    example
    setup of socket io using express generator project

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.io.emit("socketToMe", "users-----1");
+  res.send('respond with a resource.');
+});
+
+module.exports = router;
